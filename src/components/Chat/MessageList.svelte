bd00627da591594c41fc5dcd407d5e3b1d726b6d
Emupedia <admin@emupedia.net>
2020-06-25 15:35:53 +0300

    More work

---
+++ src/components/Chat/MessageList.svelte
@@ -0,0 +1,25 @@
+<style>
+	.message-container {
+		height: 100%;
+		overflow-y: auto;
+		padding: 0 10px;
+	}
+
+	.message-container p {
+		margin: 0;
+	}
+</style>
+
+<script>
+	import Message from "./Message.svelte";
+
+	export let messages = [];
+</script>
+
+<div class="message-container">
+	{#each messages as message}
+		<Message author="me" message={message.text} />
+	{:else}
+		<p>No messages</p>
+	{/each}
+</div>
