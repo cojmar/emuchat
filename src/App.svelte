e47ebf0b1245ee117a7de58fa887a1e0f1fe290d
Emupedia <admin@emupedia.net>
2020-07-10 03:17:13 +0300

    More work

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,10 @@
 <script>
 	import {Route} from 'tinro'
 	import EmuChat from './components/EmuChat.svelte'
-
-	let sys = window.$sys
-
-	let virtualScroll = !sys.browser.isIE
-	let showAvatars = !sys.browser.isEdgeHTML
-
-	console.log('showAvatars', showAvatars)
-	console.log('virtualScroll', virtualScroll)
-	console.log('sys.browser.isIE', sys.browser.isIE)
-	console.log('sys.browser.isEdgeHTML', sys.browser.isEdgeHTML)
 </script>
 
 <Route path="/*">
 	<Route path="/" redirect="/emuchat/"/>
-	<Route path="/emuchat/"><EmuChat showAvatars={showAvatars} statusJoinPart={true} virtualScroll={virtualScroll}/></Route>
+	<Route path="/emuchat/"><EmuChat showAvatars={true} statusJoinPart={true} virtualScroll={!window.$sys.browser.isIE}/></Route>
 	<Route fallback>No page found</Route>
 </Route>
